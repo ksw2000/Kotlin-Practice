91b744b308e680d75d62864af160d23320327351
liao2000 <liao2000@livemail.tw>
2021-03-12 11:18:05 +0800

    OO

--- src/main.kt
+++ src/main.kt
@@ -1,6 +1,4 @@
 fun main(args: Array<String>) {
-    println("Hello, World!")
-
     /*
     * var is like general variable and it's known as a mutable variable
     * in kotlin and can be assigned multiple times.
@@ -31,7 +29,7 @@ fun main(args: Array<String>) {
     println(str)
 
     // Arrays
-    val numbers: IntArray = intArrayOf(1,2,3)
+    val numbers: IntArray = intArrayOf(1, 2, 3)
     println("${numbers[0]}, ${numbers[1]}, ${numbers[2]}")
 
     // Collections
@@ -47,6 +45,7 @@ fun main(args: Array<String>) {
     println(set)
 
     // Range
+    // 0..2 means 0, 1, 2
     println("range demo:")
     for(i in 0..2){
         print("${numbers[i]}, ")
@@ -59,34 +58,44 @@ fun main(args: Array<String>) {
     when(x){
         1 -> print("x == 1")
         2 -> print("x == 2")
+        3, 4-> print("x == 3 || x == 4")
         else -> print("x != 1 && x != 2")
     }
 
-    when(x){
-        1,2 -> print("x == 1 or 2")
-        else -> print("x != 1 && x != 2")
-    }
-
-    // for
-    println("numbers:")
-    for(i in numbers){
-        print("$i, ")
+    // foreach
+    // use .withIndex() to get key and value
+    println("\n\nnumbers:")
+    for(value in numbers){
+        print("$value, ")
     }
 
     println()
-    for((k, v) in numbers.withIndex()){
-        println("$k->$v")
+    for((key, value) in numbers.withIndex()){
+        println("$key->$value")
     }
 
     // break, continue by using label
     println()
     val fooList: IntArray = intArrayOf(2, 4, 6, 8, 10, 12, 14)
-    l1@ for(v in fooList){
+    label1@ for(v in fooList){
         if(v > 10){
-            break@l1
+            break@label1
         }else{
             print("$v, ")
-            continue@l1
+            continue@label1
         }
     }
+    println()
+
+    // class
+    val r = Rectangle()
+    r.setWidth(10)
+    r.setLength(20)
+    println("\narea:${r.area()}")
+
+    // class with constructor
+    val c = Circle(10.0)
+    println("\narea:${c.area()}")
+
+    //
 }
