aae4a11f4199a6429c77d175b101cba292b12deb
liao2000 <liao2000@livemail.tw>
2021-06-30 11:52:06 +0800

    Update data.kt

--- src/data.kt
+++ src/data.kt
@@ -1,2 +1,6 @@
+// data classes in kotlin
+// This type of class can be used to hold the basic data apart
+// Other than this. it does not provide any other functionality
+
 data class Date(val year: Int, val month: Int, val day:Int)
 data class Person(val name: String, val birthDayYear: Date)
